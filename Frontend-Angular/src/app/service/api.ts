0271105335c98e8511981da113de5869af8f0386
Canberk Aykanat <kayra.aykanat@gmail.com>
2025-08-12 14:03:23 +0300

    Api service - angular

--- Frontend-Angular/src/app/service/api.ts
+++ Frontend-Angular/src/app/service/api.ts
@@ -48,4 +48,134 @@ export class Api {
   }
 
 
+  //Auth API
+  registerUser(body: any): Observable<any> {
+    return this.http.post(`${Api.BASE_URL}/auth/register`, body);
+  }
+
+  loginUser(body: any): Observable<any> {
+    return this.http.post(`${Api.BASE_URL}/auth/login`, body);
+  }
+
+
+   // User API
+  myProfile(): Observable<any> {
+    return this.http.get(`${Api.BASE_URL}/users/account`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  myBookings(): Observable<any> {
+    return this.http.get(`${Api.BASE_URL}/users/bookings`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  deleteAccount(): Observable<any> {
+    return this.http.delete(`${Api.BASE_URL}/users/delete`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  // Room API
+  addRoom(formData: any): Observable<any> {
+    return this.http.post(`${Api.BASE_URL}/rooms/add`, formData, {
+      headers: this.getHeader(),
+    });
+  }
+
+  updateRoom(formData: any): Observable<any> {
+    return this.http.put(`${Api.BASE_URL}/rooms/update`, formData, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getAvailableRooms(
+    checkInDate: string,
+    checkOutDate: string,
+    roomType: string
+  ): Observable<any> {
+    return this.http.get(`${Api.BASE_URL}/rooms/available`, {
+      params: { checkInDate, checkOutDate, roomType },
+    });
+  }
+
+  getRoomTypes(): Observable<any> {
+    return this.http.get(`${Api.BASE_URL}/rooms/types`);
+  }
+
+  getAllRooms(): Observable<any> {
+    return this.http.get(`${Api.BASE_URL}/rooms/all`);
+  }
+
+  getRoomById(roomId: string): Observable<any> {
+    return this.http.get(`${Api.BASE_URL}/rooms/${roomId}`);
+  }
+
+  deleteRoom(roomId: string): Observable<any> {
+    return this.http.delete(`${Api.BASE_URL}/rooms/delete/${roomId}`, {
+      headers: this.getHeader(),
+    });
+  }
+
+
+  // Booking API
+  bookRoom(booking: any): Observable<any> {
+    return this.http.post(`${Api.BASE_URL}/bookings`, booking, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getAllBookings(): Observable<any> {
+    return this.http.get(`${Api.BASE_URL}/bookings/all`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  updateBooking(booking: any): Observable<any> {
+    return this.http.put(`${Api.BASE_URL}/bookings/update`, booking, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getBookingByReference(bookingCode: string): Observable<any> {
+    return this.http.get(`${Api.BASE_URL}/bookings/${bookingCode}`);
+  }
+
+
+  // Payment API 
+  proceedForPayment(body: any): Observable<any> {
+    return this.http.post(`${Api.BASE_URL}/payments/pay`, body, {
+      headers: this.getHeader(),
+    });
+  }
+
+  updateBookingPayment(body: any): Observable<any> {
+    return this.http.put(`${Api.BASE_URL}/payments/update`, body, {
+      headers: this.getHeader(),
+    });
+  }
+
+
+
+  // Auth Check
+  logout(): void {
+    this.clearAuth();
+  }
+
+  isAuthenticated(): boolean {
+    const token = this.getFromStorageAndDecrypt('token');
+    return !!token;
+  }
+
+  isAdmin(): boolean {
+    const role = this.getFromStorageAndDecrypt('role');
+    return role === 'ADMIN';
+  }
+
+  isCustomer(): boolean {
+    const role = this.getFromStorageAndDecrypt('role');
+    return role === 'CUSTOMER';
+  }
+
 }
