ad3ff7493b474405dd05fb74c5585068759460b7
Canberk Aykanat <kayra.aykanat@gmail.com>
2025-08-12 16:55:33 +0300

    pagination component

---
+++ Frontend-Angular/src/app/roomresult/roomresult.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roomresult',
+  imports: [],
+  templateUrl: './roomresult.html',
+  styleUrl: './roomresult.css'
+})
+export class Roomresult {
+
+}
