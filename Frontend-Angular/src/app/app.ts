8d01822b3b813aa16ba970d309d26c4e7cc060d3
Canberk Aykanat <kayra.aykanat@gmail.com>
2025-08-13 11:45:05 +0300

    register page

--- Frontend-Angular/src/app/app.ts
+++ Frontend-Angular/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Footer } from './footer/footer';
+import { Register } from "./register/register";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar,Footer],
+  imports: [RouterOutlet, Navbar, Footer, Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
